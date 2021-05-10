47587e56287f9c9415bb74cf3fa7d8cbce55c4c4
DingAllen <1121174884@qq.com>
2021-05-10 13:20:54 +0800

    exceptions

--- src/lib.rs
+++ src/lib.rs
@@ -59,9 +59,3 @@ fn panic(info: &PanicInfo) -> ! {
 pub fn init() {
     interrupts::init_idt();
 }
-
-#[test_case]
-fn test_breakpoint_exception() {
-    // invoke a breakpoint exception
-    x86_64::instructions::interrupts::int3();
-}
