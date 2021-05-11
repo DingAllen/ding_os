1bb86d7c32764dc68041a7bee02666769336a9d1
DingAllen <1121174884@qq.com>
2021-05-11 17:23:29 +0800

    double_fault

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,16 @@
 
 use core::panic::PanicInfo;
 
+pub mod gdt;
 pub mod interrupts;
 pub mod serial;
 pub mod vga_buffer;
 
 pub fn init() {
+    gdt::init();
     interrupts::init_idt();
 }
+
 pub trait Testable {
     fn run(&self) -> ();
 }
